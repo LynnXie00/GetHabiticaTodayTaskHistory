d6812cb727cebb77f0cf548ee4a964ee5b346a1f
LynnXie00 <30601091+LynnXie00@users.noreply.github.com>
2023-03-13 18:02:39 -0400

    Update MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -73,6 +73,34 @@ function getTasks(user, api){
 
 //#endregion
 
+//#region <request utilities>
+function reqListener() {
+    console.log(this.responseText);
+  }
+  
+  function updateProgress(event) {
+    if (event.lengthComputable) {
+      const percentComplete = (event.loaded / event.total) * 100;
+      // ...
+    } else {
+      console.log("Unable to compute progress information since the total size is unknown.");
+    }
+  }
+  
+  function transferComplete(evt) {
+    console.log("The transfer is complete.");
+  }
+  
+  function transferFailed(evt) {
+    console.log("An error occurred while transferring the file.");
+  }
+  
+  function transferCanceled(evt) {
+    console.log("The transfer has been canceled by the user.");
+  }
+  
+  //#endregion
+  
 
 //#region <Utility Functions>
 function ParseToLocalDate(ISOstring){
